c39142f8809f6d22e8b7762da71d3de787df8850
Bhuvana K S <bhuvana.ks@rockwellautomation.com>
2024-07-04 00:39:30 +0530

    frontend files

--- messenger-app-imp/src/firebase.js
+++ messenger-app-imp/src/firebase.js
@@ -1,10 +1,10 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBOPGTHcf__1rMCVW7gnSkBZgylNAEAIVs",
-    authDomain: "messenger-app-697bf.firebaseapp.com",
-    projectId: "messenger-app-697bf",
-    storageBucket: "messenger-app-697bf.appspot.com",
-    messagingSenderId: "478262560549",
-    appId: "1:478262560549:web:d58cfd03e6090865afbccc",
-    measurementId: "G-JQX7P8GHV4"
-  };
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "apiKeyAIzaSyBOPGTHcf__1rMCVW7gnSkBZgylNAEAIVs",
+//     authDomain: "authDomainmessenger-app-697bf.firebaseapp.com",
+//     projectId: "messenger-app-697bf",
+//     storageBucket: "messenger-app-697bf.appspot.com",
+//     messagingSenderId: "478262560549",
+//     appId: "1:478262560549:web:d58cfd03e6090865afbccc",
+//     measurementId: "G-JQX7P8GHV4"
+//   };
